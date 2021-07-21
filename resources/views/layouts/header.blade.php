84daa6b6bc9127e281c3907447805fa910be082b
Patrick Chacón <patrickchacon77@hotmail.com>
2021-07-20 22:04:15 -0500

    Se agrego el nav con boostrap

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,26 @@
-<h1>Encabezado de página</h1>
+<header>
+{{-- barra de navegación  --}}
+<nav class="navbar navbar-expand-lg " aria-label="Ninth navbar example">
+<div class="container-xl">
+    <a class="navbar-brand" href="#">Container XL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample07XL">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Principal</a>
+        </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Portafolio</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Contacto</a>
+    </li>
+
+    </ul>
+    </div>
+</div>
+</nav>
+</header>
