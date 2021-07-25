a52c40523b9ef5eaabfef65265d3d645f7ec2103
Patrick Chacón <patrickchacon77@hotmail.com>
2021-07-24 21:39:26 -0500

    Avance texto e inconos del header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,28 +1,53 @@
 <header>
 {{-- barra de navegación  --}}
-<div class="container-fluid min-vw-100 min-vh-100" id="header-fondo">
-<div class="container fs-4 d-flex p-2 align-center justify-content-center ">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fw-bold">
-        <h1>Patrick Chacón</h1>
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarScroll">
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-            <li class="nav-item px-3">
-            <a class="nav-link " aria-current="page" href="#">Principal</a>
-            </li>
-            <li class="nav-item px-3">
-                <a class="nav-link " aria-current="page" href="#">Portafolio</a>
-            </li>
-            <li class="nav-item px-3">
-                <a class="nav-link " aria-current="page" href="#">Contacto</a>
-            </li>
-        </ul>
+<div class="container-fluid min-vw-100 min-vh-100 py-0" id="header-fondo">
+    <div class="container fs-5 d-flex px-2 py-0 my-2 align-center justify-content-left ">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fw-bold">
+            <h3>Patrick Chacón</h3>
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                <li class="nav-item px-3">
+                <a class="nav-link " aria-current="page" href="#">Principal</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link " aria-current="page" href="#">Portafolio</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link " aria-current="page" href="#">Contacto</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </div>
+    {{-- header-main --}}
+    <div class="container my-4 py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12  col-md-6">
+                <div class="card border-0">
+                    <H2 class="fs-4 fw-lighter">¡Hola a todos!</H2>
+                    <H1 class="fs-1">Yo soy Patrick Chacón</H1>
+                    <p class="text-uppercase fs-4">Un programador con alma de diseñador</p>
+                    <p class="text-uppercase fs-5  fw-lighter">Full-stack</p>
+                    <div class="container-fluid p-0">
+                        <a href="https://www.facebook.com/Patrickchacon7/" class="me-4"><img id="icono-facebook"></a>
+                        <a href="" class="me-4"><img id="icono-intagram"></a>
+                        <a href="" class="me-4"><img id="icono-linkerin"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12  col-md-6">
+                <div class="container">
+                    <img src="" alt="">
+                </div>
+            </div>
         </div>
     </div>
-    </nav>
 </div>
 </div>
 </header>
