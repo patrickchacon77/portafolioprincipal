079e51621a3dcc98d122dd6fafb631f3f3922051
Patrick Chacón <patrickchacon77@hotmail.com>
2021-07-25 13:53:30 -0500

    adaptación principal a responsive desing --falta

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.plantilla')
 
 @section('header')
-
+    
 @endsection
 
 
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,27 +2,8 @@
 {{-- barra de navegación  --}}
 <div class="container-fluid min-vw-100 min-vh-100 py-0" id="header-fondo">
     <div class="container fs-5 d-flex px-2 py-0 my-2 align-center justify-content-left ">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fw-bold">
-            <h3>Patrick Chacón</h3>
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                <li class="nav-item px-3">
-                <a class="nav-link " aria-current="page" href="#">Principal</a>
-                </li>
-                <li class="nav-item px-3">
-                    <a class="nav-link " aria-current="page" href="#">Portafolio</a>
-                </li>
-                <li class="nav-item px-3">
-                    <a class="nav-link " aria-current="page" href="#">Contacto</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-        </nav>
+        {{-- se incluye la barra de navegación --}}
+        @include('layouts.nav')
     </div>
     {{-- header-main --}}
     <div class="container my-4 py-5">
@@ -41,8 +22,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-12  col-md-6">
-                <div class="d-flex flex-row-reverse">
+            <div class="col-12  col-md-6 header-columan2">
+                <div class="d-flex  sm-flex-row-reverse">
                     <div class="col-">
                         <div class="card-img-top ">
                             <img class="" id="imagen-objeto1">
@@ -59,4 +40,5 @@
     </div>
 </div>
 </div>
+
 </header>
