d28900e0c4dedb365fe70c3011b4cd61e144e648
Patrick Chacón <patrickchacon77@hotmail.com>
2021-07-29 20:20:58 -0500

    Prueba de animación con cuadros

--- resources/views/layouts/portafolio.blade.php
+++ resources/views/layouts/portafolio.blade.php
@@ -28,6 +28,36 @@
                 <h2>Portafolio</h2>
             </div>
         </div>
+        <div class="row ">
+            <div class="col-12">
+                <div class="container-fondo-portafolio">
+                    <span class="objeto1 delay-1"></span>
+                    <span class="objeto2 delay-4"></span>
+                    <span class="objeto3 delay-5"></span>
+                    <span class="objeto4 delay-6"></span>
+
+                    <span class="objeto1 delay-4"></span>
+                    <span class="objeto2 delay-3"></span>
+                    <span class="objeto3 delay-2"></span>
+                    <span class="objeto4 delay-1"></span>
+
+                    <span class="objeto1 delay-3"></span>
+                    <span class="objeto2 delay-6"></span>
+                    <span class="objeto3 delay-4"></span>
+                    <span class="objeto4 delay-5"></span>
+
+                <div class="portafolio-grid p-4">   
+                    <img id="item1"  class="shadow-lg  rounded">
+                    <img id="item2"  class="shadow-lg  rounded">
+                    <img id="item3"  class="shadow-lg  rounded">
+                    <img id="item4"  class="shadow-lg  rounded">
+                    <img id="item5"  class="shadow-lg  rounded">
+                    <img id="item6"  class="shadow-lg  rounded">
+                    <img id="item7"  class="shadow-lg  rounded">
+                </div>
+            </div>
+            </div>
+        </div>
         </div>
     </div>
 </section>
