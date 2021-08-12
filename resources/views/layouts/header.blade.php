6a69d2f9edb7c9a77689b00fea828a4497cd697f
Patrick Chacón <patrickchacon77@hotmail.com>
2021-08-12 11:43:12 -0500

    cambios finales

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header>
 {{-- barra de navegación  --}}
 <div class="container-fluid  min-vh-100 py-0" id="header-fondo">
-    <div class="container fs-5 d-flex px-2 py-0 my-2 align-center justify-content-left ">
+    <div class="container fs-5 d-flex px-2 py-0 my-2 align-center justify-content-left  fixed-top sticky-md-top">
         {{-- se incluye la barra de navegación --}}
         @include('layouts.nav')
     </div>
@@ -11,21 +11,22 @@
         <div class="row">
             <div class="col-12  col-md-6">
                 <div class="card border-0">
-                    <H2 class="fs-4 fw-lighter">¡Hola a todos!</H2>
-                    <H1 class="fs-1">Yo soy Patrick Chacón</H1>
-                    <div class="d-inline-flex  p-0">
+                    <H2 class="fs-4 fw-lighter pt-3">¡Hola a todos!</H2>
+                    <H1 class="fs-1 pt-1">Yo soy Patrick Chacón</H1>
+                    <div class="d-inline-flex  p-0 pt-2">
                     <span class="escribir1"></span>
                     <p class="text-uppercase fs-4"><span class="typed"></span></p>
                     <div class="titulo" id="cadenas-texto">
-                        <p class="text-uppercase fs-4">Un <i class="profesion">Programador</i></p>
-                        <p class="text-uppercase fs-4">Un <i class="profesion">Diseñador</i></p>
-                        <p class="text-uppercase fs-4">Un <i class="profesion">Ingeniero</i></p>
+                        <p class="text-uppercase fs-4"><i class="profesion">Desarrollador</i></p>
+                        <p class="text-uppercase fs-4"><i class="profesion">Diseñador</i></p>
+                        <p class="text-uppercase fs-4"><i class="profesion">Ingeniero</i></p>
+                        <p class="text-uppercase fs-4"><i class="profesion">Docente</i></p>
                     </div>
                     <span class="escribir2"></span>
                     </div>
 
-                    <p class="text-uppercase fs-5  fw-lighter">Full-stack</p>
-                    <div class="container-fluid p-0">
+                    <p class="text-uppercase fs-5  fw-lighter pt-2">Full-stack</p>
+                    <div class="container-fluid p-0 pt-2">
                         <a href="https://www.facebook.com/Patrickchacon7/" class="me-4"><img id="icono-facebook"></a>
                         <a href="" class="me-4"><img id="icono-intagram"></a>
                         <a href="" class="me-4"><img id="icono-linkerin"></a>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,21 +1,26 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light fw-bold">
-    <h3 class="titulo-principal">Patrick Chacón</h3>
-<div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-        <li class="nav-item px-3">
-        <a class="nav-link " aria-current="page" href="#">Principal</a>
-        </li>
-        <li class="nav-item px-3">
-            <a class="nav-link " aria-current="page" href="#">Portafolio</a>
-        </li>
-        <li class="nav-item px-3">
-            <a class="nav-link " aria-current="page" href="#">Contacto</a>
-        </li>
-    </ul>
+<div class="container-nav-logo d-flex shadow rounded ">
+    <div>
+        <img class="logo p-2">
+    </div>  
+    <nav class="navbar navbar-expand-lg  navbar-light bg-light fw-bold ">
+
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+            <li class="nav-item px-3">
+            <a class="nav-link " aria-current="page" href="#">Principal</a>
+            </li>
+            <li class="nav-item px-3">
+                <a class="nav-link " aria-current="page" href="#">Portafolio</a>
+            </li>
+            <li class="nav-item px-3">
+                <a class="nav-link " aria-current="page" href="#">Contacto</a>
+            </li>
+        </ul>
+        </div>
     </div>
+    </nav>
 </div>
-</nav>
